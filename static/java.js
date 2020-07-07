8fb29693e3f7b766c226ec5f7c219c4aa1a3b043
Ammar Mohamed Sobhi <ammarmohamed13@gmail.com>
2020-07-08 01:29:51 +0200

    Update java.js

--- static/java.js
+++ static/java.js
@@ -1,4 +1,5 @@
 function writebutton(value) {
+    buttonsound();
     if (document.getElementById("equation").value.length < 50)
     {
         document.getElementById("equation").value = document.getElementById("equation").value + value ;
@@ -6,12 +7,19 @@ function writebutton(value) {
 }
 function delet()
 {
+    buttonsound();
     var len = document.getElementById("equation").value.length;
     var curequation = document.getElementById("equation").value;
     document.getElementById("equation").value = curequation.substring(0,len-1);
 }
 function reset()
 {
+    buttonsound();
     document.getElementById("equation").value = "";
 }
     
+function buttonsound()
+{
+    var audio = new Audio('/static/audio/buttonsound.mp3');
+    audio.play();
+}
